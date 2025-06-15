878820f1cba8b037207588b33056e7dce0cbff19
taanchhik <kosmos2045@inbox.ru>
2025-06-15 23:52:08 +0300

    Исправлено окрашивание букв в сетке

--- WordleSFML/GuessGrid.cpp
+++ WordleSFML/GuessGrid.cpp
@@ -87,40 +87,16 @@ void GuessGrid::checkSolution()
 	int solveCount = 0;
 	for (int i = 0; i < _solution.length(); i++) {
 		char guess = _guessLetters.at(_currentWordIndex).at(i).getLetter();
+		int requiredLetterCount = std::count(_solution.begin(), _solution.end(), guess);
 		if (guess == _solution.at(i)) {
 			_guessLetters.at(_currentWordIndex).at(i).setSolutionState(PuzzleLetter::SolutionState::CORRECT);
 			++solveCount;
 		}
+		else if (guess != _solution.at(i) && requiredLetterCount != 0) {
+			_guessLetters.at(_currentWordIndex).at(i).setSolutionState(PuzzleLetter::SolutionState::WRONG_POS);
+		}
 		else {
-			int requiredLetterCount = std::count(_solution.begin(), _solution.end(), guess);
-			if (requiredLetterCount > 0) {
-				int correctPositionThisLetter = 0;
-				std::vector<int> outOfPositionPositions;
-				for (int j = 0; j < _guessLetters.at(_currentWordIndex).size(); j++) {
-					if (guess == _guessLetters.at(_currentWordIndex).at(j).getLetter()) {
-						if (guess == _solution.at(j)) {
-							correctPositionThisLetter++;
-						}
-						else {
-							outOfPositionPositions.emplace_back(j);
-						}
-					}
-				}
-
-				while (correctPositionThisLetter + outOfPositionPositions.size() > requiredLetterCount) {
-					outOfPositionPositions.pop_back();
-				}
-				
-				if (std::find(outOfPositionPositions.begin(), outOfPositionPositions.end(), i) != outOfPositionPositions.end()) {
-					_guessLetters.at(_currentWordIndex).at(i).setSolutionState(PuzzleLetter::SolutionState::WRONG_POS);
-				}
-				else {
-					_guessLetters.at(_currentWordIndex).at(i).setSolutionState(PuzzleLetter::SolutionState::NO_STATE);
-				}
-			}
-			else {
-				_guessLetters.at(_currentWordIndex).at(i).setSolutionState(PuzzleLetter::SolutionState::NO_STATE);
-			}
+			_guessLetters.at(_currentWordIndex).at(i).setSolutionState(PuzzleLetter::SolutionState::NO_STATE);
 		}
 	}
 
