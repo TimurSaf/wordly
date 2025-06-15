2fdefe79c48f90300bdb66b33c90a761a4c2165e
taanchhik <kosmos2045@inbox.ru>
2025-06-15 22:37:41 +0300

    Добавлено 3 теста

--- WordleSFML/GuessGrid_test.cpp
+++ WordleSFML/GuessGrid_test.cpp
@@ -29,15 +29,76 @@ TEST_CASE_FIXTURE(GuessGridFixture, "Конструктор с пустым ре
 
 
 // Тесты для GuessGrid::tryInsertLetter
-TEST_CASE_FIXTURE(GuessGridFixture, "Вставка корректной буквы") {
+TEST_CASE_FIXTURE(GuessGridFixture, "Вставка корректной буквы")
+{
     GuessGrid grid(sf::IntRect(sf::Vector2<int>(0, 0), sf::Vector2<int>(800, 600)), font, "TEST", 6);
     CHECK_NOTHROW(grid.tryInsertLetter('A'));
 }
 
 
 // Тесты для GuessGrid::getAllRules
-TEST_CASE_FIXTURE(GuessGridFixture, "Получение правил для пустой сетки") {
+TEST_CASE_FIXTURE(GuessGridFixture, "Получение правил для пустой сетки")
+{
     GuessGrid grid(sf::IntRect(sf::Vector2<int>(0, 0), sf::Vector2<int>(800, 600)), font, "TEST", 6);
     auto rules = grid.getAllRules();
     CHECK(rules.empty());
+}
+
+TEST_CASE_FIXTURE(GuessGridFixture, "Правильное решение")
+{
+    GuessGrid grid(sf::IntRect(sf::Vector2<int>(0, 0), sf::Vector2<int>(800, 600)), font, "APPLE", 6);
+
+    grid.tryInsertLetter('A');
+    grid.tryInsertLetter('P');
+    grid.tryInsertLetter('P');
+    grid.tryInsertLetter('L');
+    grid.tryInsertLetter('E');
+
+    grid.checkSolution();
+
+    CHECK(grid.isSolved() == true);
+            
+    auto rules = grid.getAllRules();
+    REQUIRE(rules.size() == 1);
+    CHECK(rules[0] == "*A*P*P*L*E");
+}
+    
+
+TEST_CASE_FIXTURE(GuessGridFixture, "Частично правильное решение")
+{
+    GuessGrid grid(sf::IntRect(sf::Vector2<int>(0, 0), sf::Vector2<int>(800, 600)), font, "APPLE", 6);
+
+    grid.tryInsertLetter('A');
+    grid.tryInsertLetter('P');
+    grid.tryInsertLetter('R');
+    grid.tryInsertLetter('I');
+    grid.tryInsertLetter('L');
+
+    grid.checkSolution();
+
+    CHECK(grid.isSolved() == false);
+        
+    auto rules = grid.getAllRules();
+    REQUIRE(rules.size() == 1);
+    CHECK(rules[0] == "*A*PRI#L");
+}
+
+TEST_CASE_FIXTURE(GuessGridFixture, "Неправильное решение")
+{
+    GuessGrid grid(sf::IntRect(sf::Vector2<int>(0, 0), sf::Vector2<int>(800, 600)), font, "APPLE", 6);
+
+    grid.tryInsertLetter('B');
+    grid.tryInsertLetter('O');
+    grid.tryInsertLetter('C');
+    grid.tryInsertLetter('C');
+    grid.tryInsertLetter('I');
+
+    grid.checkSolution();
+
+    CHECK(grid.isSolved() == false);
+    CHECK(grid.hasMoreGuesses() == true);
+        
+    auto rules = grid.getAllRules();
+    REQUIRE(rules.size() == 1); 
+    CHECK(rules[0] == "BOCCI");
 }
